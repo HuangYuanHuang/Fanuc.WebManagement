cb122680894903f4289586b431bb6199560d40f8
Huang Yuan haung <dha834773759@163.com>
2019-04-28 09:45:13 +0800

    迁移mysql

--- src/Fanuc.Webmanagement.EntityFrameworkCore/EntityFrameworkCore/WebmanagementDbContextConfigurer.cs
+++ src/Fanuc.Webmanagement.EntityFrameworkCore/EntityFrameworkCore/WebmanagementDbContextConfigurer.cs
@@ -7,12 +7,12 @@ namespace Fanuc.Webmanagement.EntityFrameworkCore
     {
         public static void Configure(DbContextOptionsBuilder<WebmanagementDbContext> builder, string connectionString)
         {
-            builder.UseSqlServer(connectionString);
+            builder.UseMySql(connectionString);
         }
 
         public static void Configure(DbContextOptionsBuilder<WebmanagementDbContext> builder, DbConnection connection)
         {
-            builder.UseSqlServer(connection);
+            builder.UseMySql(connection);
         }
     }
 }
